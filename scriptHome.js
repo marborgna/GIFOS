024d6a9a6354ed6e46bff7f0e88a9015e65c0380
marborgna <marborgna8989@users.noreply.github.com>
2021-06-09 11:49:11 -0300

    Cambios carousel

--- scriptHome.js
+++ scriptHome.js
@@ -70,43 +70,48 @@ pedirGIFO();
 
 //--->carousel
 
-let slider = document.querySelector("#slider");
-let sliderSection = document.querySelectorAll(".slider-section");
-let sliderSectionLast = sliderSection[sliderSection.length -1];
-
-const btnLeft = document.querySelector("#btn-left");
-const btnRight = document.querySelector("#btn-right");
-
-slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-
-//boton derecha
-function next() {
-    let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s"; 
-    setTimeout(function() {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', sliderSectionFirst); 
-    }, 500);
-}
-
-btnRight.addEventListener('click', function() {
-    next();
-});
-
-//boton izquierda
-function prev() {
+function inicioSlider() {
+    let slider = document.querySelector("#slider");
     let sliderSection = document.querySelectorAll(".slider-section");
     let sliderSectionLast = sliderSection[sliderSection.length -1];
-    slider.style.marginLeft = "0%";
-    slider.style.transition = "all 0.5s"; 
-    setTimeout(function() {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('afterend', sliderSectionLast); 
-        slider.style.marginLeft= "-100%";
-    }, 500); 
+
+    const btnLeft = document.querySelector("#btn-left");
+    const btnRight = document.querySelector("#btn-right");
+
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+    //boton derecha
+    function next() {
+        let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+        slider.style.marginLeft = "-200%";
+        slider.style.transition = "all 0.5s"; 
+        setTimeout(function() {
+            slider.style.transition = "none";
+            slider.insertAdjacentElement('beforeend', sliderSectionFirst); 
+        }, 500);
+    }
+
+    btnRight.addEventListener('click', function() {
+        next();
+    });
+
+    //boton izquierda
+    function prev() {
+        let sliderSection = document.querySelectorAll(".slider-section");
+        let sliderSectionLast = sliderSection[sliderSection.length -1];
+        slider.style.marginLeft = "0%";
+        slider.style.transition = "all 0.5s"; 
+        setTimeout(function() {
+            slider.style.transition = "none";
+            slider.insertAdjacentElement('afterend', sliderSectionLast); 
+            slider.style.marginLeft= "-100%";
+        }, 500); 
+    }
+
+    btnLeft.addEventListener('click', function() {
+        prev();
+    });
+
 }
 
-btnLeft.addEventListener('click', function() {
-    prev();
-});
+//inicioSlider(pedirGIFO);
