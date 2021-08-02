9f2fbc796f7f57a34ab1068f0e599325f00bfe3d
marborgna <marborgna8989@users.noreply.github.com>
2021-08-02 12:50:59 -0300

    Estilo funcionalidad Sticky Navbar(falta terminar)

--- scriptHome.js
+++ scriptHome.js
@@ -1,20 +1,19 @@
 // NAV sticky
 
-var navbar = document.getElementById("navbar");
+window.onscroll = function() {stickyNav()};
 
+var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function myFunction () {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
+ //NO ADD Y SACA LA CLASS STICKY, LA DEJA SIEMPRE PueSTA
 
-window.onscroll = function() {
-    myFunction()
-};
 
 //---> llamado API
 let apiKeyGIPHY = "bdbs9mjKP9hSl2LbvfTBkpX2CDuOaHeR";
@@ -73,53 +72,8 @@ function insertarImagenSlider (url) {
 pedirGIFO();
 
 
-//--->carousel
 
-/*function inicioSlider() {
-    let slider = document.querySelector("#slider");
-    let sliderSection = document.querySelectorAll(".slider-section");
-    let sliderSectionLast = sliderSection[sliderSection.length -1];
-
-    const btnLeft = document.querySelector("#btn-left");
-    const btnRight = document.querySelector("#btn-right");
-
-    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-
-    //boton derecha
-    function next() {
-        let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
-        slider.style.marginLeft = "-200%";
-        slider.style.transition = "all 0.5s"; 
-        setTimeout(function() {
-            slider.style.transition = "none";
-            slider.insertAdjacentElement('beforeend', sliderSectionFirst); 
-        }, 500);
-    }
-
-    btnRight.addEventListener('click', function() {
-        next();
-    });
-
-    //boton izquierda
-    function prev() {
-        let sliderSection = document.querySelectorAll(".slider-section");
-        let sliderSectionLast = sliderSection[sliderSection.length -1];
-        slider.style.marginLeft = "0%";
-        slider.style.transition = "all 0.5s"; 
-        setTimeout(function() {
-            slider.style.transition = "none";
-            slider.insertAdjacentElement('afterend', sliderSectionLast); 
-            slider.style.marginLeft= "-100%";
-        }, 500); 
-    }
-
-    btnLeft.addEventListener('click', function() {
-        prev();
-    });
-
-}*/
-
-// carousel 2
+// Carousel
 
 var posicionSlider = 0;
 
